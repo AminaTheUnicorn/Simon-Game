f1842adeca04a9fd3a1d306ab386db3abad475bc
Aminat.A <aminatadesola@gmail.com>
2020-08-19 01:52:01 -0500

    starting files

---
+++ game.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    let buttonColours = ["red", "blue", "green", "yellow"];
+    let gamePattern = [];
+
+    function nextSequence(){
+
+        let randomNumber = Math.floor(Math.random() * 4);
+        let randomChosenColour = buttonColours[randomNumber]
+        gamePattern.push(randomChosenColour);
+    }
+    
+ 
+ 
+  });
